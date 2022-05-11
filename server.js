f156f6f3cfb82485778d7a2cebe7eabb84be4110
Alberto Méndez <alberto.mwt@gmail.com>
2022-05-11 18:23:01 -0500

    database

---
+++ server.js
@@ -0,0 +1,31 @@
+//express
+const express = require('express');
+//Mongo
+const bodyParser = require('body-parser');
+//Models and routes
+const Task = require('./api/models/todoList.model'); //Created model loading here
+const routes = require('./api/routes/todoList.routes');
+//dotenv
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const app = express();
+//Mongoose instance connection url connection
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.bmx50.mongodb.net/${process.env.NAME}?retryWrites=true&w=majority`;
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app); //register the route
+
+//Spin up server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`todo list RESTful API server started on ${PORT}`);
+});
